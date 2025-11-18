37cf968d74a4483da12871b62897ea4a5f451e49
c0a24189d4 <c0a24189d4@edu.teu.ac.jp>
2025-11-18 17:23:32 +0900

    練習10:キーボード操作

--- pygame_image.py
+++ pygame_image.py
@@ -13,23 +13,33 @@ def main():
     bg_img_2 = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png") 
     kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        bg_x = tmr
+
+        bg_x = tmr % 3200
         screen.blit(bg_img, [-bg_x, 0])
         screen.blit(bg_img_2, [-bg_x + 1600, 0])
         screen.blit(bg_img, [-bg_x+3200, 0])
-        screen.blit(kk_img, [300, 200])
+
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0, -1))
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0, 1))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1, 0))
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((1, 0))
+
+        screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
-        
-        if tmr > 3199:
-            tmr = 0
-
 
 if __name__ == "__main__":
     pg.init()
